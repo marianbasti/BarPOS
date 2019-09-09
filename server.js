3d81da63a46f8948d31e50c889e60283f29c0b57
marixer <31198560+marixer@users.noreply.github.com>
2019-09-09 20:19:51 -0300

    Ordenes casi terminadas, notificaciones sonoras :100:
    Y estilos varios

--- server.js
+++ server.js
@@ -28,19 +28,40 @@ app.get('/tables.json', function (req, res) {
   res.sendFile(__dirname + '/tables.json');
 });
 
+app.get('/order.mp3', function (req, res) {
+  res.sendFile(__dirname + '/order.mp3');
+});
+
+app.get('/open.mp3', function (req, res) {
+  res.sendFile(__dirname + '/open.mp3');
+});
+
+app.get('/closed.mp3', function (req, res) {
+  res.sendFile(__dirname + '/closed.mp3');
+});
+
 io.on('connection', function(socket){
   socket.on('tableopened', function(table){
     var json = JSON.stringify(table[0]);
     console.log('Opened table ' + table[1]);
     fs.writeFile('tables.json', json, 'utf8', function callback(err) {
     });
-    socket.broadcast.emit('tableopenedres', table[1]);
+    io.emit('tableopenedres', table[1]);
   });
   socket.on('tableclosed', function(table){
+    table[0][table[1]].orders = [];
     var json = JSON.stringify(table[0]);
     console.log('Closed table ' + table[1]);
     fs.writeFile('tables.json', json, 'utf8', function callback(err) {
     });
-    socket.broadcast.emit('tableclosedres', table[1]);
+    io.emit('tableclosedres', table[1]);
+  });
+  socket.on('alteredorders', function(table){
+    var json = JSON.stringify(table[0]);
+    console.log('Altered table: ' + table[1]);
+    console.log('Orders: ' + table[0][table[1]-1].orders);
+    fs.writeFile('tables.json', json, 'utf8', function callback(err) {
+    });
+    io.emit('alteredordersres', [table[1],table[0][table[1]-1].orders]);
   });
 })
